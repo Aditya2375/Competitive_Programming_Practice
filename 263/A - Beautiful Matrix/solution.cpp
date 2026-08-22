869b1569d14586bc5cdb59072b02571da3f4f23c
Aditya2375 <adityakulkarni2708@gmail.com>
2026-08-22 17:02:37 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+    int x, y;
+ 
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            int a;
+            cin >> a;
+ 
+            if (a == 1) {
+                x = i;
+                y = j;
+            }
+        }
+    }
+ 
+    cout << abs(x - 3) + abs(y - 3) << endl;
+ 
+    return 0;
+}
